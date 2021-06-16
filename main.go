b0fdd810c4537053fbd9a6d09fd046c5b19a3307
vincentzreo <1978892854@qq.com>
2021-06-16 14:46:32 +0800

    Add user.go

--- main.go
+++ main.go
@@ -1,10 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	colour "github.com/fatih/color"
+	flag "github.com/ogier/pflag"
+	"os"
+	"strings"
+)
+
+var (
+	user  string
+	emoji string
+)
 
 func main() {
 	// 运行 go run main.go
 	// 建造 go build
 	// 安装 go install
-	fmt.Println("Hello, World")
+	flag.Parse()
+	if flag.NFlag() == 0 {
+		printUsage()
+	}
+	users := strings.Split(user, ",")
+	fmt.Printf("Searching user(s): %s\n", users)
+	fmt.Println("")
+	for _, u := range users {
+		result := getUesrs(u)
+		colour.Cyan(`Username: %s`, result.Login)
+		colour.Blue(`Name:			%s`, result.Name)
+		colour.Green(`Email:			%s`, result.Email)
+		colour.HiMagenta(`Bio:		%s`, result.Bio)
+		fmt.Println("")
+	}
+}
+
+func init() {
+	flag.StringVarP(&user, "user", "u", "", "Search Users")
+}
+func printUsage() {
+	fmt.Printf("Usage: %s [options]\n", os.Args[0])
+	fmt.Println("Options:")
+	flag.PrintDefaults()
+	os.Exit(1)
 }
